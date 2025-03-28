0c35fd2a58ba279948b5eb9957372ade5ead2ba5
Florencia Pereira <fpereira@spacedev.uy>
2025-03-28 16:17:39 -0300

    fix types

--- src/cloud-storage/abstract/cloud-storage-provider.interface.ts
+++ src/cloud-storage/abstract/cloud-storage-provider.interface.ts
@@ -5,21 +5,25 @@ export interface ICloudStorageProvider {
   /**
    * Uploads a file to the Cloud Storage provider.
    * @param {Express.Multer.File} file - The file to upload.
-   * @returns {string} The file identifier in the cloud provider.
+   * @returns {Promise<{ url: string; id: string }>} An object containing:
+   *   - `url`: The public URL where the file can be accessed.
+   *   - `id`: The unique identifier assigned to the file in the cloud provider.
    */
   uploadFile(file: Express.Multer.File): Promise<{ url: string; id: string }>;
 
   /**
    * Delete a file from the Cloud Storage provider.
-   * @param {Express.Multer.File} file - The file to upload.
+   * @param {string} fileKey - The unique identifier assigned to the file in the cloud provider.
    * @returns {void}
    */
   deleteFile(fileKey: string): Promise<void>;
 
   /**
    * Get a file from the Cloud Storage provider.
-   * @param {Express.Multer.File} file - The file to upload.
-   * @returns {string} The file url in the cloud provider.
+   * @param {string} fileKey - The unique identifier assigned to the file in the cloud provider.
+   * @returns {Promise<{ url: string; id: string }>} An object containing:
+   *   - `url`: Signed URL where the file can be accessed.
+   *   - `id`: The unique identifier assigned to the file in the cloud provider.
    */
   getFile(fileKey: string): Promise<{ url: string; id: string }>;
 }
