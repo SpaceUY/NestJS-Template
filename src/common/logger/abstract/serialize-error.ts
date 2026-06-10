61703011fffa3e9337d672c58dfb210baeceebaf
Franco Mangone <francomangone18@gmail.com>
2026-06-10 19:26:00 -0300

    chore: yet another sonar comment

--- src/common/logger/abstract/serialize-error.ts
+++ src/common/logger/abstract/serialize-error.ts
@@ -31,7 +31,7 @@ export function serializeError(
   }
 
   try {
-    return JSON.parse(JSON.stringify(error)) as Record<string, unknown>;
+    return structuredClone(error) as Record<string, unknown>;
   } catch {
     return Object.prototype.toString.call(error);
   }
